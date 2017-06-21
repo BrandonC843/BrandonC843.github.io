1690fa552b859846b1643818d629be3c1fe018b4
Brandon Callahan <brandoncallahan@Brandons-MBP.attlocal.net>
2017-06-21 08:58:37 -0400

    added different examples

--- js/app.js
+++ js/app.js
@@ -1,2 +1,14 @@
-window.alert("welcome home budddy!!!!")
-console.log("whats up")
+
+
+console.log("THIS IS WHERE THE MAGIC HAPPENS")
+
+var pageVisit = Math.floor(Math.random() * 125) + 1
+var realNumber = Math.ceil(Math.random() * 25) + 1
+alert("Welcome! Of the " + pageVisit + " of visits, You are by far " + " #" + realNumber + ".")
+
+console.log("!++!++!++!++!++!++!");
+var input = prompt("Whoa Whoa Whoa! stop the clock");
+var stopnumb = parseInt(input);
+var randomnumb = Math.floor(Math.random() * stopNumb) + 1
+var message = "<p>" + randomnumb + " is a number between 1 and " + stopNumb + ".</p>";
+document.write(message);
