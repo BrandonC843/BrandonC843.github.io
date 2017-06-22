8702c48753c673e98cbd5874bd363a8b1e000476
Brandon Callahan <brandoncallahan@Brandons-MBP.attlocal.net>
2017-06-22 11:41:36 -0400

    images

--- js/app.js
+++ js/app.js
@@ -7,8 +7,3 @@ var realNumber = Math.ceil(Math.random() * 25) + 1
 alert("Welcome! Of the " + pageVisit + " of visits, You are by far " + " #" + realNumber + ".")
 
 console.log("!++!++!++!++!++!++!");
-var input = prompt("Whoa Whoa Whoa! stop the clock");
-var stopnumb = parseInt(input);
-var randomnumb = Math.floor(Math.random() * stopNumb) + 1
-var message = "<p>" + randomnumb + " is a number between 1 and " + stopNumb + ".</p>";
-document.write(message);
