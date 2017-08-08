0d59fe3a555bf47d46f4824ca53349f44682bf00
Brandon Callahan <brandoncallahan@Brandons-MBP.attlocal.net>
2017-08-07 20:45:01 -0400

    complete overhaul

--- js/app.js
+++ js/app.js
@@ -1,24 +1,2 @@
 
-
 console.log("THIS IS WHERE THE MAGIC HAPPENS")
-//
-// var pageVisit = Math.floor(Math.random() * 125) + 1
-// var realNumber = Math.ceil(Math.random() * 25) + 1
-// alert("Welcome! Of the " + pageVisit + " of visits, You are by far " + " #" + realNumber + ".")
-//
-
-Just some  JavaScript fun //
-var language = prompt("welcome, what is the greatest sport in the world?")
-if(language.toUpperCase() === "HURLING"){
-   window.alert("Wow that's impressive!")
-}else{
-   window.alert("OH NO! it's actually Hurling!")
-}
-console.log("Hurling is an ancient Irish field sport that has aspects of soccer, baseball, lacrosse, and field hockey")
-
-
-// var imageSwap function = mouseOver(){
-//    document.getElementById("wallPic").style.background{
-//
-//    }
-// }
