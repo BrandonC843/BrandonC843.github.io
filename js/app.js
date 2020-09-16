84ad44ed759d4fcf7d3c77a5ca06589494908f8d
Brandon Callahan <b_callahan843@.cloud.com>
2020-09-16 14:08:55 -0400

    pages working

--- js/app.js
+++ js/app.js
@@ -1,3 +1,65 @@
 
 console.log("THIS IS WHERE THE MAGIC HAPPENS")
 console.log("redit ud itidudtd duuu");
+
+
+function showNavbar (){
+   document.getElementById("navbar").style.display - "inline-block";
+};
+setTimeout("showNavbar()", 1000);
+
+
+
+
+
+
+
+// const navScroll = window.pageYOffset;
+// function window.onScroll => {
+//    var currentPosition = window.pageYOffset;
+//    if (navScroll > currentPosition){
+//       document.getElementById("navbar").style.top = "0";
+//    } else {
+//       document.getElementById("navbar").style.top = "-50px";
+//    }
+//    navScroll = currentPosition;
+// };
+// var displayDate = new Date();
+// var hours = displayDate.getHours();
+//
+// var message;
+//
+// if(hours < 10){
+//    message = 'Good Morning, ';
+// }else if (hours <20){
+//    message = 'Good Day, ';
+// }else {
+//    message = 'Good Evening, ';
+// };
+//
+// document.getElementById('greeting').innerHTML = message;
+//
+// window.addEventListener('click', () => {
+//    console.log('what are you doing this for?');
+// })
+//
+//
+// // Calendar funtion //
+// var date = new Date();
+// var day = date.getDate();
+// var year = date.getFullYear();
+//
+// var month = date.getMonth();
+// var monthList = ["January", "February", "March", "April", "May", "June", "July", "August", "Sept", "October", "November", "December"];
+//
+// var weekDay = date.getDay();
+// var weekDayList = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+//
+// function Day(){
+//    month = monthList[month];
+//    weekDay = weekDayList[weekDay];
+// document.getElementById("date").innerHTML = weekDay + ",  " + month + day;
+// };
+// Day();
+
+// expanding icon //
