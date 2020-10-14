45ce0af39fa5374093b82a6702b7b85807757886
Brandon Callahan <b_callahan843@.cloud.com>
2020-10-14 13:57:10 -0400

    more styling and adding information on who page

--- js/app.js
+++ js/app.js
@@ -1,65 +1,24 @@
 
 console.log("THIS IS WHERE THE MAGIC HAPPENS")
-console.log("redit ud itidudtd duuu");
+const navTrick = document.querySelector('grr');
 
+var displayDate = new Date();
+var hours = displayDate.getHours();
+var message;
 
-function showNavbar (){
-   document.getElementById("navbar").style.display - "inline-block";
+if(hours < 10){
+   message = 'Good Morning, I hope your day has started great';
+}else if (hours <20){
+   message = 'Good Day, please dont forget to eat lunch. I recommend Primos Hoagies';
+}else {
+   message = 'Good Evening, make sure you brush your teeth before bed.';
 };
-setTimeout("showNavbar()", 1000);
-
-
-
-
-
-
-
-// const navScroll = window.pageYOffset;
-// function window.onScroll => {
-//    var currentPosition = window.pageYOffset;
-//    if (navScroll > currentPosition){
-//       document.getElementById("navbar").style.top = "0";
-//    } else {
-//       document.getElementById("navbar").style.top = "-50px";
-//    }
-//    navScroll = currentPosition;
-// };
-// var displayDate = new Date();
-// var hours = displayDate.getHours();
-//
-// var message;
-//
-// if(hours < 10){
-//    message = 'Good Morning, ';
-// }else if (hours <20){
-//    message = 'Good Day, ';
-// }else {
-//    message = 'Good Evening, ';
-// };
-//
-// document.getElementById('greeting').innerHTML = message;
-//
+console.log(message);
 // window.addEventListener('click', () => {
-//    console.log('what are you doing this for?');
+//    console.log(message);
 // })
 //
-//
-// // Calendar funtion //
-// var date = new Date();
-// var day = date.getDate();
-// var year = date.getFullYear();
-//
-// var month = date.getMonth();
-// var monthList = ["January", "February", "March", "April", "May", "June", "July", "August", "Sept", "October", "November", "December"];
-//
-// var weekDay = date.getDay();
-// var weekDayList = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-//
-// function Day(){
-//    month = monthList[month];
-//    weekDay = weekDayList[weekDay];
-// document.getElementById("date").innerHTML = weekDay + ",  " + month + day;
-// };
-// Day();
-
+// navTrick.addEventListener('mouseover', () =>{
+//    console.log(navTrick);
+// });
 // expanding icon //
---
+++ js/who.js
@@ -0,0 +1,16 @@
+console.log('Page connected');
+window.setTimeout((something) =>{
+   console.log(something);
+}, 8000, "Having dreams is what makes life tolerable");
+
+
+// const listItems = document.getElementById('#heading');
+//
+// for(var i = 0; i < listItems.length; i += 1) {
+//   listItems[i].addEventListener('mouseover', () => {
+//   listItems[i].textContent[i] = listItems[i].textContent.toUpperCase();
+// });
+// listItems[i].addEventListener('mouseout', () => {
+//   listItems[i].textContent = listItems.textContent.toLowerCase();
+// });
+// };
